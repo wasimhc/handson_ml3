a1cd2a7d6c759066da9a3ffed7bb326207a1dfb2
Wasim Haider Chowdhury <wasimhc@gmail.com>
2024-08-02 15:12:08 +0600

    Machine Learning Landscape

--- 1-1-prediction.py
+++ 1-1-prediction.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 
 # Download and prepare the data
-data_root = "https://github.com/ageron/data/raw/main/"
+data_root = "https://github.com/agilecodex/handson_ml3/blob/master/data/main/"
 lifesat = pd.read_csv(data_root + "lifesat/lifesat.csv")
 X = lifesat[["GDP per capita (USD)"]].values
 y = lifesat[["Life satisfaction"]].values
@@ -22,5 +22,5 @@ model = LinearRegression()
 model.fit(X, y)
 
 # Make a prediction for Cyprus
-X_new = [[37_655.2]]  # Cyprus' GDP per capita in 2020
+X_new = [[6_1720]]  # India' GDP per capita in 2020
 print(model.predict(X_new)) # outputs [[6.30165767]]
